36551c1d90b531b99ce5e858011b98be9f61249f
cahyadhis <cahyadhis@gmail.com>
2021-03-11 22:54:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 <body>
         <h1>hello world!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dignissimos non culpa nisi officiis similique eaque itaque accusamus tempore doloribus in harum hic laboriosam voluptas excepturi, modi magnam quidem sed qui! Alias recusandae officiis tenetur exercitationem aliquid ab dignissimos in optio, libero quos dolores porro nostrum saepe, obcaecati vitae esse repudiandae odit rem unde quam! Veritatis magni soluta dignissimos tempore quaerat! Neque quo, excepturi aliquam voluptas incidunt facere obcaecati iusto cum animi autem reprehenderit repellat? Eligendi ipsum similique voluptatem veritatis ut praesentium deserunt animi, maiores nobis dignissimos omnis ad minus amet laborum quam mollitia, reprehenderit eaque nesciunt, recusandae neque! Voluptatem, deserunt quas illum voluptate reprehenderit repellat et, error saepe debitis modi accusantium reiciendis. Accusantium ea veritatis voluptate fugiat dolore pariatur nihil dicta neque minima qui. Tenetur laboriosam dolor temporibus dolorum cum assumenda veniam. Repellat nam eaque dignissimos maiores recusandae aspernatur similique omnis, laborum provident! Blanditiis magni dolorem accusantium nesciunt natus neque quam doloribus iusto aliquam corrupti amet explicabo esse dicta corporis obcaecati, maxime, voluptatem voluptates ab dolor, non minima nisi qui porro! Repellat quasi facere, aut iure aliquam nesciunt fugit beatae eius labore placeat cupiditate consectetur magnam hic architecto nam magni totam odit suscipit eum veniam quia tempore? Sint, reprehenderit?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    
+    </ul>
     
 </body>
 </html>
